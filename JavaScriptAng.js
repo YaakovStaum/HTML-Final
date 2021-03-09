4548fdb35bb4cab0837e6d772bf6eba27fc37a17
Yaakov Staum <yakstaum@gmail.com>
2021-03-09 22:47:19 +0200

    clarifying/labeling Angular items in solution

---
+++ JavaScriptAng.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("app", [])
+        .controller("storecontroller", function ($http) {
+            var vm = this;
+
+            vm.loadingMask = loadingMask;
+            vm.downloadProds = downloadProds;
+
+            init();
+
+            function init() { };
+
+            function downloadProds() {
+                return $http({
+                    url: "json.json",
+                    method: "get"
+                })
+                    .then(function (products) {
+
+                    })
+            }
+        })
+})
