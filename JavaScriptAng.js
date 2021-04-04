7ffccee91ad932382b7f089a665926105a7675f4
Yaakov Staum <yakstaum@gmail.com>
2021-04-04 23:42:18 +0300

    seprating code into stylesheet javascript and index

--- JavaScriptAng.js
+++ JavaScriptAng.js
@@ -1,11 +1,15 @@
 ﻿angular
     .module("app", [])
     .controller("StoreController", function ($http) {
-        var vm = this;        
+        var vm = this;
+
+        vm.popUpForm = popUpForm;
+        vm.closeShowForm = closeShowForm;
+        vm.reviews = reviews;
+        vm.closeReviewShower = closeReviewShower;
+        vm.submitReview = submitReview;
 
         init();
-
-
 
         function downloadProds() {
             return $http({
@@ -17,7 +21,34 @@
                 });
         };
 
+        function popUpForm(productId) {
+            vm.curComment = {};
+            vm.showForm = true;
+            vm.curComment.productId = productId;
+        };
+
+        function closeShowForm() {
+            vm.showForm = false;
+        };
+
+        function submitReview() {
+            vm.curComment.date = new Date();
+            vm.myProdReviews.push(vm.curComment);
+            vm.curComment = {};
+            closeShowForm();
+        };
+
+        function reviews(product) {
+            vm.reviewShower = true;
+            vm.reviewProd = product;
+        };
+
+        function closeReviewShower() {
+            vm.reviewShower = false;
+        };
+
         function init() {
             downloadProds();
+            vm.myProdReviews = [];
         };
     });
