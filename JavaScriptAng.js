f5ce108082c0c6d110090e5cf4a24422ea429eff
Yaakov Staum <yakstaum@gmail.com>
2021-05-01 23:52:35 +0300

    Creatin Arrays for form inputs

--- JavaScriptAng.js
+++ JavaScriptAng.js
@@ -18,29 +18,35 @@
             })
                 .then(function (httpResponse) {
                     vm.products = httpResponse.data;
-                });
+                })
         };
 
-        function popUpForm(productId) {
-            vm.curComment = {};
-            vm.showForm = true;
+        function arrays() {
+            vm.ProdReviews = new Array();
+            vm.ProdReviewsEdit = new Array();
+        };
+
+        function popUpForm(productId) {    
+            vm.showForm = true;            
             vm.curComment.productId = productId;
+            vm.productId = productId;
         };
 
-        function closeShowForm() {
+        function closeShowForm() {            
             vm.showForm = false;
+            vm.curComment = {};
         };
 
         function submitReview() {
             vm.curComment.date = new Date();
-            vm.myProdReviews.push(vm.curComment);
-            vm.curComment = {};
+            vm.ProdReviews.push(vm.curComment);
             closeShowForm();
         };
 
-        function reviews(product) {
+        function reviews(product, productId) {
             vm.reviewShower = true;
             vm.reviewProd = product;
+            vm.productId = productId;
         };
 
         function closeReviewShower() {
@@ -48,7 +54,8 @@
         };
 
         function init() {
-            downloadProds();
-            vm.myProdReviews = [];
+            vm.curComment = {};
+            downloadProds()
+                .then(arrays);           
         };
     });
